37610be91aae319f4062ef91ca2f001fb4c06c75
Nikolay0803 <battmandu@gmail.com>
2023-11-07 16:25:09 +0200

    commit fifeth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Button from "../button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
-  const { user, OnClose } = useTelegram();
+  const { user, onClose } = useTelegram();
   return (
     <div className={"header"}>
       <Button onClick={onClose}>Закрити</Button>
